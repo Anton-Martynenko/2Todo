89dc426df675b9ff4dafcc58da12635c1c75ad91
Молодец 2705 <ant_papa@mail.ru>
2023-02-09 11:56:08 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import './App.css';
+import {Todolist} from './Todolist';
+import {v1} from "uuid";
+
+export type FilterValuesType = 'all' | 'active' | 'completed';
+
+function App() {
+
+    let initTasks = [
+        {id: v1(), title: "HTML&CSS", isDone: true},
+        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "ReactJS", isDone: false},
+        {id: v1(), title: "Rest API", isDone: false},
+        {id: v1(), title: "GraphQL", isDone: false}
+    ]
+
+    let [tasks, setTasks] = useState(initTasks);
+
+    function removeTask(id: string) {
+        let filteredTasks = tasks.filter(task => task.id !== id)
+        setTasks(filteredTasks)
+    }
+
+
+    let [filter, setFilter] = useState<FilterValuesType>( 'all' )
+    let tasksForTodolist = tasks
+     if (filter === 'active') {
+         tasksForTodolist = tasks.filter(task => task.isDone === false)
+     }
+     if (filter === 'completed') {
+         tasksForTodolist = tasks.filter(task => task.isDone === true)
+     }
+
+     function changeFilter(value: FilterValuesType) {
+         setFilter(value)
+     }
+
+    return (
+        <div className="App">
+            <Todolist title="What to learn"
+                      tasks={tasksForTodolist}
+                      removeTask={removeTask}
+                      changeFilter={changeFilter}
+            />
+
+        </div>
+    );
+}
+
+export default App;
