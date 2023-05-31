2e28c9383a4147efbea53d934897069384c96da5
Молодец 2705 <ant_papa@mail.ru>
2023-05-31 10:20:41 +0600

    changeTlFilter test

--- src/state/todolists-Reducer.ts
+++ src/state/todolists-Reducer.ts
@@ -14,6 +14,20 @@ export const todolistsReducer = (state: Array<TodolistsType>, action: ActionType
         case 'ADD-TODOLIST': {
             return [...state, { id: v1(), title: action.title, filter: "all" }]
         }
+        case 'CHANGE-TODOLIST-TITLE': {
+            let todolist = state.find(t => t.id === action.id);
+            if (todolist) {
+                todolist.title = action.title;
+            }
+            return [...state];
+        }
+        case 'CHANGE-TODOLIST-FILTER': {
+            let todolist = state.find(tl => tl.id === action.id);
+            if (todolist) {
+                todolist.filter = action.filter;
+            }
+            return [...state];
+        }
         default:
             throw new Error('I don\'t understand this type')
     }
